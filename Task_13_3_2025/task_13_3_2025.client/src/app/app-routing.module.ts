3c6610e8e9ba94932b6e240dafce13a8dd4adec8
MohammadAlshorman <mhmdshhadhalshrman95@gmail.com>
2025-03-12 16:17:20 +0300

    Add project files.

---
+++ Task_13_3_2025/task_13_3_2025.client/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignComponent } from './sign/sign.component';
+import { RegComponent } from './reg/reg.component';
+import { CategoryComponent } from './category/category.component';
+
+const routes: Routes = [
+
+  //{ path: 'nav-bar', component: NavBarComponent },
+  //{ path: 'home', component: HomeComponent },
+  //{ path: 'sign-in', component: SignInComponent },
+  //{ path: 'sign-up', component: SignUpComponent },
+  //{ path: 'category', component: CategoryComponent },
+  //{ path: 'product', component: ProductComponent },
+  //{ path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
+
+  { path: 'nav', component: NavComponent },
+  { path: 'foooter', component: FooterComponent },
+  { path: 'sign', component: SignComponent },
+  { path: 'reg', component: RegComponent },
+  { path: 'category', component: CategoryComponent },
+
+
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
