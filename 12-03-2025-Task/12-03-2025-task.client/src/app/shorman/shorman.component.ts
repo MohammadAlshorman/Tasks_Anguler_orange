1ef19a607867a4a53918ae72cb8c3ce3964f8d1e
MohammadAlshorman <mhmdshhadhalshrman95@gmail.com>
2025-03-16 20:44:15 +0300

    add task (*-*)

---
+++ 12-03-2025-Task/12-03-2025-task.client/src/app/shorman/shorman.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shorman',
+  templateUrl: './shorman.component.html',
+  styleUrl: './shorman.component.css'
+})
+export class ShormanComponent {
+
+}
